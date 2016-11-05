66da45795c21825be33f6620acb1147ec1fd838a
github username <github email address>
2016-11-05 18:33:31 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function breakOut(array, changeValue, stopValue){
+  for (var i = 0; i<array.length; i ++){
+    if (array[i] != stopValue){
+    array[i] = changeValue;
+  }
+    else {
+      return array;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue){
+  for (var i = 0; i<array.length; i ++){
+    if (array[i] != skipValue){
+    array[i] = changeValue;
+  }
+    else {
+      array[i] = array[i];
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+  return null
+}
